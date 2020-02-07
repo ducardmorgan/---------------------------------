3280590e972417845129ef9ac7915e80c6f035c7
Ducard Morgan <59864467+ducardmorgan@users.noreply.github.com>
2020-02-07 14:42:45 +0500

    .

--- Интерактивные приложения/11/a2.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	$date = date("Y")*1;
-	$year = $_REQUEST['year']*1;
-	$fio = $_REQUEST['fio'];
-	$date = $date - $year;
-	echo json_encode(array("year"=>$year,"date"=>$date,"fio"=>$fio));
-?>
